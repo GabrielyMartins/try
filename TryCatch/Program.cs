a2e30c5055465c05fccf662e224a365ea1d1e503
Gabriely Maritns <gabrielybudim@gmail.com>
2023-08-08 15:15:55 -0400

    primeiro commit

---
+++ TryCatch/Program.cs
@@ -0,0 +1,27 @@
+﻿try
+{
+    Console.WriteLine("Informe o primeiro valor!");
+    int a = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Informe o segundo valor!");
+    int b = Convert.ToInt32(Console.ReadLine());
+
+    int r = a / b;
+    Console.WriteLine($"O resultado é {r}");
+
+}catch(DivideByZeroException e) 
+{
+    Console.WriteLine("Não é permitido divisão por zero!" + e.Message);
+}
+catch(InvalidCastException e)
+{
+    Console.WriteLine("Não é permitido divisão por letras!" + e.Message);
+}
+catch(Exception e)
+{
+    Console.WriteLine("Ocorreu um erro!" + e.Message);
+}
+finally
+{
+    Console.WriteLine("Aqui passa de qualquer forma!  ");
+}
